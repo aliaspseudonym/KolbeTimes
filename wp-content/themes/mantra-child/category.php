d226009d227f070aaa795c7b08d19ef560dcd6a1
aliaspseudonym <alias13th@gmail.com>
2015-04-20 01:19:44 -0600

    Mobile styling

--- wp-content/themes/mantra-child/category.php
+++ wp-content/themes/mantra-child/category.php
@@ -1,66 +1,67 @@
-<?php /*
-Template Name: Custom Category Page
-*/ ?>
+<?php
+/**
+ * The template for displaying Category Archive pages.
+ *
+ * @package Cryout Creations
+ * @subpackage Mantra
+ * @since Mantra 1.0
+ */
 
+get_header(); ?>
 
-<?php get_header(); ?>
+		<section id="container">
+			<div id="content" role="main">
+			<?php cryout_before_content_hook(); ?>
+			
+			<?php if ( have_posts() ) : ?>
 
-<section id="container">
-	<div id="content" role="main">
+				<header class="page-header">
+					<h1 class="page-title"><?php
+						printf( __( 'Category Archives: %s', 'mantra' ), '<span>' . single_cat_title( '', false ) . '</span>' );
+					?></h1>
 
-	 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<span class="mobile-only"><a href="<?php the_permalink(); ?>"><h1 class="entry-title"></a><?php the_title(); ?></h1></span>
-			<div class="entry-content">
-				<div class="feat-image">
-					<?php the_post_thumbnail('medium'); ?>
-				</div>
-				<div class="category-excerpt">
-					<a href="<?php the_permalink(); ?>"><h1 class="entry-title"><?php the_title(); ?></h1></a>
-					<span>By 
-						<?php 
-							if (get_the_author_posts() == 1) {
-								the_author(); ?>. <?php the_tags(); ?></span>
-							<?php } else { 
-								the_author_posts_link(); ?>. <?php the_tags(); ?></span>
-						<?php } ?>
-					<?php the_excerpt(); ?>
+					<?php
+						$category_description = category_description();
+						if ( ! empty( $category_description ) )
+							echo apply_filters( 'category_archive_meta', '<div class="category-archive-meta">' . $category_description . '</div>' );
+					?>
+				</header>
 
-					<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'mantra' ), 'after' => '</div>' ) ); ?>
-					<?php edit_post_link( __( 'Edit', 'mantra' ), '<span class="edit-link">', '</span>' ); ?>
-				</div>
-			</div> <!-- entry-content -->
-			<div style="clear: both;"></div>
-		</div>
-		<?php
-		$slug = basename(get_permalink());
-		$meta_slug = get_post_meta(get_the_ID(), "slug", $single); // slug custom field
-		$meta_catid = get_post_meta(get_the_ID(), "catid", $single); // category_id custom field
-		$key = get_post_meta(get_the_ID(), "key", $single); // either slug or category_id custom field
-		$slug = ($key?$key:($meta_catid?$meta_catid:($meta_slug?$meta_slug:($slug?$slug:0)))); // select one value out of the custom fields 	
-		?>
-	<?php endwhile; else: endif; ?>
-	<hr>
-	<br />
-	<?php 
-	$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-	if (is_numeric($slug)&&($slug>0)): 
-		$the_query = new WP_Query( 'cat='.$slug.'&post_status=publish&orderby=date&order=desc&posts_per_page='.get_option('posts_per_page').'&paged=' . $paged ); 
-	else: 
-		$the_query = new WP_Query( 'category_name='.$slug.'&post_status=publish&orderby=date&order=desc&posts_per_page='.get_option('posts_per_page').'&paged=' . $paged ); 
-	endif; 
-	/* Start the Loop */ 
-	while ( $the_query->have_posts() ) : $the_query->the_post();
-		global $more; $more=0; // more gets lost inside page templates
-		get_template_part( 'content', get_post_format() ); 
-	endwhile;
-	if($mantra_pagination=="Enable") mantra_pagination($the_query->max_num_pages); else mantra_content_nav( 'nav-below' );	
-	?>
-		
-	</div><!-- #content -->
-	
-	<?php get_sidebar(); ?>
-	
-</section><!-- #container -->
+				<?php mantra_content_nav( 'nav-above' ); ?>
+
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php
+						/* Include the Post-Format-specific template for the content.
+						 * If you want to overload this in a child theme then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part( 'content', get_post_format() );
+					?>
+
+				<?php endwhile; ?>
+
+					<?php if($mantra_pagination=="Enable") mantra_pagination(); else mantra_content_nav( 'nav-below' ); ?>
+
+			<?php else : ?>
+
+				<article id="post-0" class="post no-results not-found">
+					<header class="entry-header">
+						<h1 class="entry-title"><?php _e( 'Nothing Found', 'mantra' ); ?></h1>
+					</header><!-- .entry-header -->
+
+					<div class="entry-content">
+						<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'mantra' ); ?></p>
+						<?php get_search_form(); ?>
+					</div><!-- .entry-content -->
+				</article><!-- #post-0 -->
+
+			<?php endif; ?>
+			
+			<?php cryout_after_content_hook(); ?>
+			</div><!-- #content -->
+		<?php get_sidebar(); ?>
+		</section><!-- #primary -->
 
 <?php get_footer(); ?>
