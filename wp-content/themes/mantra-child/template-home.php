2fbe7419b9630f77adf6ad850f164c864ec1c831
aliaspseudonym <alias13th@gmail.com>
2015-05-02 13:17:53 -0600

    Final homepage tweaks

--- wp-content/themes/mantra-child/template-home.php
+++ wp-content/themes/mantra-child/template-home.php
@@ -15,7 +15,7 @@
 get_header(); ?>
 
 		<section id="container" class="one-column">
-			<div id="content" role="main">
+			<div id="content" roles="main">
 <span class="noshow">
 			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
@@ -44,27 +44,27 @@ get_header(); ?>
 	data-cycle-pause-on-hover="true"
     >
 	<div>
-		<a class="link" href="<?php echo home_url('/'); ?>857/">
+		<a class="link" href="<?php echo home_url('/'); ?>i-am-immigrant/">
 			<img src="<?php echo home_url('/'); ?>wp-content/uploads/2015/04/I-Am-Immigrant-SLIDER.jpg"/>
 		</a>
 	</div>
 	<div>
-		<a class="link" href="<?php echo home_url('/'); ?>calgary-dream-centre-a-focus-on-compassion/">
+		<a class="link" href="<?php echo home_url('/'); ?>the-language-of-grief/">
 			<img src="<?php echo home_url('/'); ?>wp-content/uploads/2015/04/Language-of-Grief-SLIDER.jpg"/>
 		</a>
 	</div>
 	<div>
-		<a class="link" href="<?php echo home_url('/'); ?>corps-bara-youth-dance-company-mercy/">
+		<a class="link" href="<?php echo home_url('/'); ?>journey-through-dementia-my-mothers-words/">
 			<img src="<?php echo home_url('/'); ?>wp-content/uploads/2015/04/Journey-Through-Dementia-SLIDER.jpg"/>
 		</a>
 	</div>
 	<div>
-		<a class="link" href="<?php echo home_url('/'); ?>once-you-know-the-spirit-of-mercy-in-rwanda/">
+		<a class="link" href="<?php echo home_url('/'); ?>why-words-matter/">
 			<img src="<?php echo home_url('/'); ?>wp-content/uploads/2015/04/Why-Words-Matter-SLIDER.jpg"/>
 		</a>
 	</div>
 		<div>
-		<a class="link" href="<?php echo home_url('/'); ?>857/">
+		<a class="link" href="<?php echo home_url('/'); ?>words-of-healing-words-of-hope/">
 			<img src="<?php echo home_url('/'); ?>/wp-content/uploads/2015/04/Words-of-Healing-SLIDER.jpg"/>
 		</a>
 	</div>
@@ -86,8 +86,8 @@ get_header(); ?>
 			
 
 <div id="quote">
-	<h1>"We live by revelation, as Christians, as artists ... which means we must be careful never to get set into rigid molds."</h1>
-	<p>-Madeleine L'Engle</p>
+	<h1>Tell us about one of your favourite books, and why it made an impact. We’d love to share your picks with our readers!</h1>
+	<a href="<?php echo home_url('/'); ?>submissions/"><p>Click here to submit!</p></a>
 </div> <!-- #quote -->
 
 			
@@ -101,21 +101,21 @@ get_header(); ?>
 			<div>
 				<br />
 				<h1>Book Reviews</h1>
-				<h2>Books that Changed My Life</h2>
-				<p>Dr. Gordon Smith discusses a series of books which had a formative impact on his life.</p>
+				<a href="<?php echo home_url('/'); ?>books-that-changed-my-life/"><h2>Books that Changed My Life</h2></a>
+				<p>Dr. Gordon Smith discusses the books that had a significant impact on his faith formation, and led to a change of heart.</p>
 				<p><a href="<?php echo home_url('/'); ?>category/written-word/book-reviews/">All Book Reviews</a></p>
 			</div>
-			<img src="<?php echo home_url('/'); ?>wp-content/uploads/2015/04/books.jpg"/>
+			<a href="<?php echo home_url('/'); ?>books-that-changed-my-life/"><img src="<?php echo home_url('/'); ?>wp-content/uploads/2015/04/books.jpg"/></a>
 		</div>
 		<div>
 			<div>
 				<br />
 				<h1>Featured Artist</h1>
-				<h2>Rob Hudec</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				<a href="<?php echo home_url('/'); ?>featured-artist-rob-hudec/"><h2>Rob Hudec</h2></a>
+				<p>Meet Rob Hudec, who has gathered together his musical friends in a joyful new gospel album.</p>
 				<p><a href="<?php echo home_url('/'); ?>category/featured-artist/">All Featured Artists</a></p>
 			</div>
-			<img src="<?php echo home_url('/'); ?>wp-content/uploads/2015/04/The-Narrow-Road-cover_3.jpg"/>
+			<a href="<?php echo home_url('/'); ?>featured-artist-rob-hudec/"><img src="<?php echo home_url('/'); ?>wp-content/uploads/2015/05/featuredartistthumb.jpg"></a>
 		</div>
 	</div>
 	<div class="column">
@@ -123,22 +123,21 @@ get_header(); ?>
 			<div>
 				<br />
 				<h1>Poetry</h1>
-				<h2>Valentine for Ernest Mann</h2>
-				<p>You can’t order a poem like you order a taco. / Walk up to the counter, say, “I’ll take two”
-				</p>
+				<a href="<?php echo home_url('/'); ?>valentine-for-ernest-mann/"><h2>Valentine for Ernest Mann</h2></a>
+				<p>A poem by poet, songwriter and novelist Naomi Shihab Nye, who brings a fresh, playful perspective to everyday life.</p>
 				<p><a href="<?php echo home_url('/'); ?>category/written-word/poetry/">All Poetry</a></p>
 			</div>
-			<img src="<?php echo home_url('/'); ?>wp-content/uploads/2014/11/mc_logo_new.png"/>
+			<a href="<?php echo home_url('/'); ?>valentine-for-ernest-mann/"><img src="<?php echo home_url('/'); ?>wp-content/uploads/2015/05/poetrythumb.jpg"/></a>
 		</div>
 		<div>
 			<div>
 				<br />
 				<h1>Film Reviews</h1>
-				<h2>Letters to Father Jacob</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				<a href="<?php home_url('/'); ?>film-review-letters-to-father-jacob"><h2>Letters to Father Jacob</h2></a>
+				<p>Bill Locke reviews a film from Finland that teams up two unlikely protagonists in a moving story of redemption and hope.</p>
 				<p><a href="<?php echo home_url('/'); ?>category/film/film-reviews/">All Film Reviews</a></p>
 			</div>
-			<img src="<?php echo home_url('/'); ?>wp-content/uploads/2015/04/Letters-to-Father-Jacob-COVER-CROP.jpg"/>
+			<a href="<?php home_url('/'); ?>film-review-letters-to-father-jacob"><img src="<?php echo home_url('/'); ?>wp-content/uploads/2015/05/FilmReviewsThumb.jpg"/></a>
 		</div>
 	</div>
 	<div class="column">
@@ -147,7 +146,7 @@ get_header(); ?>
 				<br />
 				<h1>Editorials</h1>
 				<h2>Musings from the Editor's Desk</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+				<p>Which words stick in our minds, and why? This issue of Kolbe Times explores the power of words.</p>
 				<p><a href="#">All Editorials</a></p>
 			</div>
 			<img src="<?php echo home_url('/'); ?>wp-content/uploads/2015/04/words.jpg"/>
@@ -155,12 +154,12 @@ get_header(); ?>
 		<div>
 			<div>
 				<br />
-				<h1>Theatre</h1>
-				<h2>Miracle Worker</h2>
-				<p>Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-				<p><a href="<?php echo home_url('/'); ?>category/performance-arts/theatre/">All Theatre Articles</a></p>
+				<h1>Guest Columnists</h1>
+				<a href="<?php echo home_url('/'); ?>dialogue-true-openness-steadfast-convictions/"><h2>Dialogue</h2></a>
+				<p>Deacon Adrian Martens reflects on the nature of true dialogue, which opens new doors while honouring parameters.</p>
+				<p><a href="<?php echo home_url('/'); ?>category/guest-columnists/">All Guest Columnists</a></p>
 			</div>
-			<img src="<?php echo home_url('/'); ?>wp-content/uploads/2015/04/Miracle-Worker-image.jpg"/>
+			<img src="<?php echo home_url('/'); ?>wp-content/uploads/2015/05/GuestColumnistThumb.jpg"/>
 		</div>
 	</div>
 </div> <!-- grid -->
@@ -168,21 +167,21 @@ get_header(); ?>
 <div id="writers">
 	<div class="writers-column-1">
 		<div>
-			<h1>Gathered Wisdom</h1>
-			<img src="<?php echo home_url('/'); ?>wp-content/uploads/2015/04/gathered-wisdom-thumb.jpg" />
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+			<a href="<?php echo home_url('/'); ?>gathered-wisdom/"><h1>Gathered Wisdom</h1>
+			<img src="<?php echo home_url('/'); ?>wp-content/uploads/2015/04/gathered-wisdom-thumb.jpg" /></a>
+			<p>A self-confessed quotations freak shares some of the insightful quotes that have fed her soul.</p>
 		</div>
 		<div>
-			<h1>Dialogue</h1>
-			<img src="<?php echo home_url('/'); ?>wp-content/uploads/2015/04/Dialogue-thumb.jpg" />
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+			<a href="<?php echo home_url('/'); ?>theatre-review-miracle-worker/"><h1>Miracle Worker</h1>
+			<img src="<?php echo home_url('/'); ?>wp-content/uploads/2015/04/MiracleWorkerThumb.jpg" /></a>
+			<p>Meghan Konditi reviews Rosebud’s production of how Helen Keller was freed from her dark, silent world as a deaf and mute young girl.</p>
 		</div>
 	</div> <!-- writers-column -->
 	<div class="writers-column-2">
 		<div>
-			<h1>A Beautiful Synergy</h1>
-			<img src="<?php echo home_url('/'); ?>wp-content/uploads/2015/04/synergy-thumb.jpg" />
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+			<a href="<?php echo home_url('/'); ?>a-beautiful-synergy/"><h1>A Beautiful Synergy</h1>
+			<img src="<?php echo home_url('/'); ?>wp-content/uploads/2015/04/synergy-thumb.jpg" /></a>
+			<p>Renate and Keith Worthington are a talented couple who combine language and art…with fun, flair and grace.</p>
 		</div>
 	</div> <!-- writers-column -->
 </div> <!-- writers -->
